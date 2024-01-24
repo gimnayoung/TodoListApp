fdb9d8b1df3feccd8de29ccd04a803a84e545b45
nayoung <kny_7_@naver.com>
2024-01-24 18:26:05 +0900

    todo app v1

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,11 +1,23 @@
 import React from "react";
+import TodoItem from "../components/TodoItem"
 
-const TodoBoard = () => {
+const TodoBoard = ({todoList,deleteItem,toggleComplete}) => {
   return (
     <div>
       <h2>Todo List</h2>
-      {/* <TodoItem/> will be here once we get the todoList */}
-      <h2>There is no Item to show</h2>
+      <div>
+      {
+        todoList.length > 0 &&
+        (todoList.map((item,index) =>
+          <TodoItem 
+          key={index} 
+          item={item}
+          deleteItem={deleteItem}
+          toggleComplete={toggleComplete}
+          />))
+      } 
+      {/* 렌더링 되지않아서 key값을 넣었더니 활성화됨 */}
+      </div>
     </div>
   );
 };
